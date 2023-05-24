2863050e86bcee98a600d6baf92fa8f8013a9d58
David Rivera <drtaupier@gmail.com>
2023-05-23 22:32:44 -0400

    Se elimino el css por no afectar a nada

--- src/app/auth/pages/main/main.component.ts
+++ src/app/auth/pages/main/main.component.ts
@@ -4,28 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styles: [`
-    .wrapper{
-      background-color: #94929D;
-      height: 100vh;
-    }
-    .formulario{
-      height: 90vh;
-    }
-    .footer{
-      width: 100vw;
-      height: 10vh;
-      background-color: #1F2B50;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-
-    .footer-container p{
-      color: #F9F5F5;
-    }
-  `
-  ]
+  styles: []
 })
 export class MainComponent implements OnInit {
 
