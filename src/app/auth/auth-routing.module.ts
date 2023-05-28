894d1ab42fe677bc824e0b7cd08800a315237fd9
David Rivera <drtaupier@gmail.com>
2023-05-28 00:01:12 -0400

    Se modifico el componente para el login

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
 import { MainComponent } from './pages/main/main.component';
 import { RegisterComponent } from './pages/register/register.component';
 
@@ -9,7 +8,7 @@ const routes: Routes = [
     path: '',
     component: MainComponent,
     children: [
-      { path: 'login', component: LoginComponent },
+      { path: 'login', component: MainComponent },
       { path: 'register', component: RegisterComponent },
       { path: '**', redirectTo: 'login' },
     ]
