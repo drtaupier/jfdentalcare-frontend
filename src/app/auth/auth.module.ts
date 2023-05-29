c862ba9e9e8a2c7585bd3aef0dd54cec87625491
David Rivera <drtaupier@gmail.com>
2023-05-29 08:23:23 -0400

    Se eliminó login component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
 import { MainComponent } from './pages/main/main.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent,
     RegisterComponent,
     MainComponent
   ],
