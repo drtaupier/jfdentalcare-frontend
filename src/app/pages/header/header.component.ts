164a2c9b5b28a8622d8e8bcac57ffd39303fe31d
David Rivera <drtaupier@gmail.com>
2026-09-16 16:24:24 -0400

    Redesign navigation header

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,34 +1,18 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  sticky = false;
+export class HeaderComponent {
   menuOpen = false;
 
-  constructor() { }
-
-  ngOnInit(): void { }
-
-  @HostListener('window:scroll', ['$event'])
-  checkScroll() {
-    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    this.sticky = scrollPosition >= 10;
-
-    // agrega la clase "sticky-header" al body si el header es pegajoso, quita la clase en caso contrario
-    if (this.sticky) {
-      document.body.classList.add('navbar-sticky');
-    } else {
-      document.body.classList.remove('navbar-sticky');
-    }
-  }
-
-  toggleMenu(){
+  toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
   }
+
+  closeMenu(): void {
+    this.menuOpen = false;
+  }
 }
-
-
