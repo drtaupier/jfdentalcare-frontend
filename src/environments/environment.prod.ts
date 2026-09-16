87710be7f2229f5d3558ce19f35ab5039986eb6d
David Rivera <drtaupier@gmail.com>
2026-09-16 11:37:56 -0400

    Fix production API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'jfdentalcare-g7svr-postgres.jfdentalcare-g7svr.svc.cluster.local:5432'
+  baseUrl: 'https://jfdentalcare1-8hvzx.kinsta.app',
 };
